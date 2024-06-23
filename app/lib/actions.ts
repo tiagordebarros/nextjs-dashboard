dc974c686ce6c9efa8473ea2b52e0b0ff8748b41
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-06-22 23:11:27 -0300

    refactor: implement sql query on createInvoice server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -18,7 +19,15 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
+
   // Test it out:
-  console.log(rawFormData);
-  console.log(typeof rawFormData.amount);
+  // console.log(rawFormData);
+  // console.log(typeof rawFormData.amount);
 }
