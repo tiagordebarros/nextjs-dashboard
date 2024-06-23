f36b8f9be74e24799d6b05303aa22e38166e62f1
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2024-06-22 23:15:34 -0300

    refactor: add revalidatePath and redirect to createInvoice server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -26,6 +28,9 @@ export async function createInvoice(formData: FormData) {
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
+
+revalidatePath('/dashboard/invoices');
+redirect('/dashboard/invoices');
 
   // Test it out:
   // console.log(rawFormData);
